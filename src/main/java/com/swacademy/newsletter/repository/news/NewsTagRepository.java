fff3f0785906f9dff12acba76c169382f95fe8ca
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-28 23:51:31 +0900

    refactor : 패키지 이동

--- src/main/java/com/swacademy/newsletter/repository/news/NewsTagRepository.java
+++ src/main/java/com/swacademy/newsletter/repository/news/NewsTagRepository.java
@@ -1,4 +1,4 @@
-package com.swacademy.newsletter.repository;
+package com.swacademy.newsletter.repository.news;
 
 import com.swacademy.newsletter.domain.tag.NewsTag;
 import org.springframework.data.jpa.repository.JpaRepository;
