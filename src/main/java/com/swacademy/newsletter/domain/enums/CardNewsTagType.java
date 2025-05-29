c8e2da79a28ed8e0e8c9f9b62d16b66c4047e5bd
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-30 00:28:04 +0900

    feat : CardNewsTag, Type Enum displayName 추가

--- src/main/java/com/swacademy/newsletter/domain/enums/CardNewsTagType.java
+++ src/main/java/com/swacademy/newsletter/domain/enums/CardNewsTagType.java
@@ -1,11 +1,20 @@
 package com.swacademy.newsletter.domain.enums;
 
+import lombok.Getter;
+
 // 하나의 게시글에 들어갈 수 있는 관련 내용 Tag
+@Getter
 public enum CardNewsTagType {
-    animal,
-    plant,
-    food,
-    policy,
-    science,
-    life
+    animal("동물"),
+    plant("식물"),
+    food("음식"),
+    policy("정책"),
+    science("과학"),
+    life("생활");
+
+    private final String displayName;
+
+    CardNewsTagType(String displayName) {
+        this.displayName = displayName;
+    }
 }
--- src/main/java/com/swacademy/newsletter/domain/enums/CardNewsType.java
+++ src/main/java/com/swacademy/newsletter/domain/enums/CardNewsType.java
@@ -1,8 +1,17 @@
 package com.swacademy.newsletter.domain.enums;
 
+import lombok.Getter;
+
 // 뉴스 자체의 type
+@Getter
 public enum CardNewsType {
-    urgent,    // 긴급
-    practice,  // 실천
-    daily   // 일상
+    urgent("긴급"),
+    practice("실천"),
+    daily("일상");
+
+    private final String displayName;
+
+    CardNewsType(String displayName) {
+        this.displayName = displayName;
+    }
 }
