84841de1a72ac1617182729090a7bca266192121
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-30 00:31:09 +0900

    refactor : CardNewsListController 삭제 및 이동

--- src/main/java/com/swacademy/newsletter/web/controller/news/CardNewsController.java
+++ src/main/java/com/swacademy/newsletter/web/controller/news/CardNewsController.java
@@ -1,11 +1,16 @@
 package com.swacademy.newsletter.web.controller.news;
 
 import com.swacademy.newsletter.apiPayload.ApiResponse;
+import com.swacademy.newsletter.converter.CardNewsConverter;
+import com.swacademy.newsletter.domain.cardnews.CardNews;
+import com.swacademy.newsletter.domain.enums.CardNewsType;
 import com.swacademy.newsletter.service.news.CardNewsQueryService;
 import com.swacademy.newsletter.service.news.generation.CardNewsGenerationService;
+import com.swacademy.newsletter.service.news.list.CardNewsListService;
 import com.swacademy.newsletter.web.dto.request.generation.GenerateCardNewsRequestDto;
 import com.swacademy.newsletter.web.dto.response.cardnews.CardNewsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +20,7 @@ public class CardNewsController {
 
     private final CardNewsQueryService cardNewsQueryService;
     private final CardNewsGenerationService generationService;
-
+    private final CardNewsListService cardNewsListService;
 
     @GetMapping("/{cardNewsId}/detail")
     public ApiResponse<CardNewsResponseDto.CardNewsDetailResultDto> getCardNewsDetail(
@@ -26,10 +31,27 @@ public class CardNewsController {
         );
     }
 
+    @GetMapping
+    public ApiResponse<CardNewsResponseDto.CardNewsListResultDto> getCardNewsList(
+            @RequestParam("type") CardNewsType type,
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "size", defaultValue = "10") Integer size
+    ) {
+
+        Slice<CardNews> cardNewsList = cardNewsListService.getCardNewsList(
+                type,
+                page,
+                size
+        );
+        return ApiResponse.onSuccess(CardNewsConverter.toListResponseDto(cardNewsList));
+    }
+
     @PostMapping("/generate")
     public CardNewsResponseDto.GenerateCardNewsResultDto generateCardNews(
             @RequestBody GenerateCardNewsRequestDto request
     ) {
         return generationService.generateCardNews(request);
     }
+
+
 }
